53073edb2a939ec2553f9d664f993b5ff45d6f40
Angel-Emmanuel-6174 <20253rd023@utez.edu.mx>
2026-03-27 13:14:08 -0600

    Se agregaron metodos en el bancoController

--- src/main/java/com/example/banco/BancoController.java
+++ src/main/java/com/example/banco/BancoController.java
@@ -1,4 +1,24 @@
 package com.example.banco;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
 public class BancoController {
+    @FXML
+    private Label lblres;
+    double saldo = 1000;
+
+    @FXML
+    protected void depositar() {
+        lblres.setText("DEPOSITAR");
+    }
+    @FXML
+    protected void retirar() {
+        lblres.setText("DEPOSITAR");
+    }
+    @FXML
+    protected void consultar() {
+        lblres.setText("DEPOSITAR");
+    }
 }
